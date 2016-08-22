ad1cd2b8a7530d2eb2e04629fbfba2f4e2157b50
Gregor Woiwode <gw@co-it.eu>
2016-08-22 21:34:41 +0200

    Displays total count of books

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,12 +12,19 @@ export class DashboardComponent implements OnInit {
 
   books: Array<Book>;
 
+  get booksCount(): number { return this.books.length; }
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.books = [
       new Book('Angular 2', '<p>Angular</p> 2 kommt bald'),
       new Book('Aurelia', 'Die Konkurrenz =)')
     ];
+  }
+
+  // Methode
+  count(): number {
+    return this.books.length;
   }
 }
