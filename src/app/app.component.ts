7b0254ff5bc00332d8dc86be934cb49fe16bbf54
Gregor Woiwode <gw@co-it.eu>
2016-08-22 15:17:37 +0200

    Displays two books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from './dashboard';
 
 @Component({
   selector: 'app-root', // <app-root>
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [DashboardComponent]
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'app-dashboard', // <app-dashboard>
   templateUrl: 'dashboard.component.html',
   styleUrls: ['dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
+  books: Array<string>;
+
   constructor() { }
 
   ngOnInit() {
+    this.books = ['Angular 2 kommt bald', 'AngularJS war mal.'];
   }
-
 }
