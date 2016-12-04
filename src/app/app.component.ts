d1fbb1d9a02fc282c292617f8dcfa932e3b438ef
Gregor Woiwode <gw@co-it.eu>
2016-12-04 21:02:52 +0100

    Fixes validation visualization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root', // <app-root>
-  templateUrl: 'app.component.html',
-  encapsulation: ViewEncapsulation.None
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   title = 'app works!';
